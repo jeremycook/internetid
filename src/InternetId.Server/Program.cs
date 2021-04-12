3057fded792b5806eb2a3008872db63e7bb87456
Jeremy Cook <jeremycook.co@gmail.com>
2021-04-12 05:51:16 -0600

    APPSETTINGS environment variable for providing a JSON configuration file to a container

--- src/InternetId.Server/Program.cs
+++ src/InternetId.Server/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
 
 namespace InternetId.Server
 {
@@ -10,6 +13,16 @@ namespace InternetId.Server
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(options => options.UseStartup<Startup>());
+                .ConfigureWebHostDefaults(options => options
+                    .UseStartup<Startup>()
+                    .ConfigureAppConfiguration((webHostBuilderContext, configurationBuilder) =>
+                    {
+                        if (Environment.GetEnvironmentVariable("APPSETTINGS") is string configPath)
+                        {
+                            configPath = Path.GetFullPath(configPath);
+                            configurationBuilder.AddJsonFile(configPath, optional: false, reloadOnChange: true);
+                        }
+                    })
+                );
     }
 }
