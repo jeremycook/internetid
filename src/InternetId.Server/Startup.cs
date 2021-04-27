4d14787303bd5407deea78d01db510eac48fb415
Jeremy Cook <jeremycook.co@gmail.com>
2021-04-27 11:12:29 -0600

    HTTPS redirect and HSTS should be handled by reverse proxy in prod

--- src/InternetId.Server/Startup.cs
+++ src/InternetId.Server/Startup.cs
@@ -69,11 +69,7 @@ namespace InternetId.Server
             {
                 app.UseStatusCodePagesWithReExecute("/error");
                 //app.UseExceptionHandler("/error");
-
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                //app.UseHsts();
             }
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseSerilogRequestLogging(o => o.EnrichDiagnosticContext = Program.SerilogEnrichFromRequest);
