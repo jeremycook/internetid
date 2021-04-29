12e77e46161155e973e539a140fd3df0279636f5
Jeremy Cook <jeremycook.co@gmail.com>
2021-04-29 13:29:31 -0600

    Compensate for OpenID Connect client to handle same site strict

--- samples/InternetId.MvcClient/Startup.cs
+++ samples/InternetId.MvcClient/Startup.cs
@@ -1,11 +1,13 @@
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 
 namespace InternetId.MvcClient
 {
@@ -13,7 +15,7 @@ namespace InternetId.MvcClient
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCookiePolicy(options => options.MinimumSameSitePolicy = Microsoft.AspNetCore.Http.SameSiteMode.Strict);
+            services.AddCookiePolicy(options => options.MinimumSameSitePolicy = SameSiteMode.Strict);
 
             services.AddAuthentication(options =>
             {
@@ -76,6 +78,43 @@ namespace InternetId.MvcClient
 
             app.UseRouting();
 
+            app.Use(async (ctx, next) =>
+            {
+                var schemes = ctx.RequestServices.GetRequiredService<IAuthenticationSchemeProvider>();
+                var handlers = ctx.RequestServices.GetRequiredService<IAuthenticationHandlerProvider>();
+                foreach (var scheme in await schemes.GetRequestHandlerSchemesAsync())
+                {
+                    var handler = await handlers.GetHandlerAsync(ctx, scheme.Name) as IAuthenticationRequestHandler;
+                    if (handler != null && await handler.HandleRequestAsync())
+                    {
+                        // start same-site cookie special handling
+                        string location = null;
+                        if (ctx.Response.StatusCode == 302)
+                        {
+                            location = ctx.Response.Headers["location"];
+                        }
+                        else if (ctx.Request.Method == "GET" && !ctx.Request.Query["skip"].Any())
+                        {
+                            location = ctx.Request.Path + ctx.Request.QueryString + "&skip=1";
+                        }
+
+                        if (location != null)
+                        {
+                            ctx.Response.StatusCode = 200;
+                            var html = $@"
+                        <html><head>
+                            <meta http-equiv='refresh' content='0;url={location}' />
+                        </head></html>";
+                            await ctx.Response.WriteAsync(html);
+                        }
+                        // end same-site cookie special handling
+
+                        return;
+                    }
+                }
+
+                await next();
+            });
             app.UseAuthentication();
             app.UseAuthorization();
 
