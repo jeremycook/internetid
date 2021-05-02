84903528aea16dcb86b3013801abcdef92e18c68
Jeremy Cook <jeremycook.co@gmail.com>
2021-05-01 18:25:37 -0600

    Add /connect/clients/create form

---
+++ src/InternetId.Server/Areas/Connect/Controllers/ClientsController.cs
@@ -0,0 +1,60 @@
+﻿using InternetId.Server.Areas.Connect.Views;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OpenIddict.Abstractions;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace InternetId.Server.Areas.Connect.Controllers
+{
+    [Authorize]
+    [Area("Connect")]
+    [Route("[area]/[controller]")]
+    public class ClientsController : Controller
+    {
+        private readonly ILogger<ClientsController> logger;
+        private readonly IOpenIddictApplicationManager openIddictApplicationManager;
+
+        public ClientsController(
+            ILogger<ClientsController> logger,
+            IOpenIddictApplicationManager openIddictApplicationManager)
+        {
+            this.logger = logger;
+            this.openIddictApplicationManager = openIddictApplicationManager;
+        }
+
+        [HttpGet("[action]")]
+        public ActionResult Create()
+        {
+            var input = new CreateViewModel();
+            return View(input);
+        }
+
+        [HttpPost("[action]")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Required] CreateViewModel input)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var application = input.ToOpenIddictApplicationDescriptor();
+
+                    var result = await openIddictApplicationManager.CreateAsync(application);
+                    logger.LogError("{User} created the {ClientId} OpenID Connect client", User.Identity!.Name, application.ClientId);
+
+                    return View("Created", model: application);
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, $"Suppressed {ex.GetType()}: {ex.Message}");
+                    ModelState.AddModelError("", "An error occurred, and your changes were not saved.");
+                }
+            }
+
+            return View(input);
+        }
+    }
+}
---
+++ src/InternetId.Server/Areas/Connect/Views/Clients/CreateViewModel.cs
@@ -0,0 +1,57 @@
+﻿using OpenIddict.Abstractions;
+using System;
+using System.ComponentModel.DataAnnotations;
+using static OpenIddict.Abstractions.OpenIddictConstants;
+
+namespace InternetId.Server.Areas.Connect.Views
+{
+    public class CreateViewModel
+    {
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Name")]
+        public string? Name { get; set; }
+
+        [Required]
+        [RegularExpression("^https://.+", ErrorMessage = "The {0} must begin with \"https://\".")]
+        [Display(Name = "Redirect URL")]
+        public string? RedirectUrl { get; set; }
+
+        [Required]
+        [RegularExpression("^https://.+", ErrorMessage = "The {0} must begin with \"https://\".")]
+        [Display(Name = "Post logout return URL")]
+        public string? PostLogoutRedirectUrl { get; set; }
+
+        /// <summary>
+        /// Create a new <see cref="OpenIddictApplicationDescriptor"/> based on <c>this</c>.
+        /// </summary>
+        /// <returns></returns>
+        public OpenIddictApplicationDescriptor ToOpenIddictApplicationDescriptor()
+        {
+            return new OpenIddictApplicationDescriptor
+            {
+                DisplayName = Name,
+                RedirectUris = { new Uri(RedirectUrl!) },
+                PostLogoutRedirectUris = { new Uri(PostLogoutRedirectUrl!) },
+                ClientId = Guid.NewGuid().ToString(),
+                ClientSecret = Guid.NewGuid().ToString(),
+                ConsentType = ConsentTypes.Explicit,
+                Permissions =
+                {
+                    Permissions.Endpoints.Authorization,
+                    Permissions.Endpoints.Logout,
+                    Permissions.Endpoints.Token,
+                    Permissions.GrantTypes.AuthorizationCode,
+                    Permissions.GrantTypes.RefreshToken,
+                    Permissions.ResponseTypes.Code,
+                    Permissions.Scopes.Email,
+                    Permissions.Scopes.Profile,
+                },
+                Requirements =
+                {
+                    Requirements.Features.ProofKeyForCodeExchange
+                }
+            };
+        }
+    }
+}
